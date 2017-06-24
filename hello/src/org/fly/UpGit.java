e87b3f09136ed250e435c3f67a4b5222538e4680
nooverfly <532967158@qq.com>
2017-06-24 10:30:48 +0800

    hello

---
+++ hello/src/org/fly/UpGit.java
@@ -0,0 +1,8 @@
+package org.fly;
+
+public class UpGit {
+
+	public static void main(String[] args) {
+		System.out.println("hello,github");
+	}
+}
--- xiangmufenghui/target/fh-1.0-SNAPSHOT/staticfile/js/common.js
+++
@@ -1,8 +0,0 @@
-/* 打开一个新页面：调用时不加第二个参数 add by tony
- * 'start','_self'*/
-function formSubmit (url,sTarget){
-    document.forms[0].target = sTarget;//target:在哪里展现新页面,_self:在当前页面展示.
-    document.forms[0].action = url;		//相对路径
-    document.forms[0].submit();			//表单元素提交
-    return true;
-}
